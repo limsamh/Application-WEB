b3282fd966e34905fe4350fcdd23ad9ca06c0404
limsamh <limsamohamed@yahoo.fr>
2016-04-03 15:09:58 +0200

    Commentaires

--- Cat/src/main/java/com/neko/controller/ChatServelt.java
+++ Cat/src/main/java/com/neko/controller/ChatServelt.java
@@ -1,7 +1,11 @@
 package com.neko.controller;
 
 import javax.servlet.http.HttpServlet;
-
+/**
+ * Controleur permettant de gerer toute les action concernant un chat
+ * 
+ *
+ */
 public class ChatServelt extends HttpServlet
 {
 	
