b627f923fe64427ad0a125a16586abba216484f1
mbarrelinovic <LM@MBARRE-PC>
2016-04-03 14:04:08 +0200

    bd v2

--- Cat/src/main/resources/chatbd.sql
+++ Cat/src/main/resources/chatbd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Dim 03 Avril 2016 à 01:25
+-- Généré le :  Dim 03 Avril 2016 à 14:02
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -51,6 +51,7 @@ CREATE TABLE IF NOT EXISTS `chat` (
   `image` varchar(40) NOT NULL,
   `type_chat` int(11) NOT NULL,
   `login` varchar(20) DEFAULT NULL,
+  `couleur` varchar(30) NOT NULL,
   PRIMARY KEY (`nom_chat`),
   KEY `fk_login1` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -59,24 +60,56 @@ CREATE TABLE IF NOT EXISTS `chat` (
 -- Contenu de la table `chat`
 --
 
-INSERT INTO `chat` (`nom_chat`, `nom_japonais`, `description`, `caractere`, `niveau`, `image`, `type_chat`, `login`) VALUES
-('Billy the Kitten', 'きっどさん', 'Western Wear', 'Nihilistic', 250, 'images/16.png', 0, NULL),
-('Bob the Cat', 'やまねこさん', 'Adventurer', 'Outdoorsy', 40, 'images/7.png', 0, NULL),
-('Chairman Meow', 'なべねこさん', 'Camouflage', 'Boorish', 111, 'images/4.png', 0, NULL),
-('Conductor Whiskers', 'えきちょうさん', 'Railway Uniform', 'Vigilant', 50, 'images/8.png', 0, NULL),
-('Frosty', 'ゆきねこさん', 'Straw Coat', 'Sensitive', 5, 'images/17.png', 0, NULL),
-('Guy Furry', 'びすとろさん', 'Apron', 'Artisan', 30, 'images/12.png', 0, NULL),
-('Joe DiMeowgio', 'たてじまさん', 'Baseball Jersey', 'Team Player', 28, 'images/1.png', 0, NULL),
-('Kathmandu', 'まろまゆさん ', 'Hunting Coat', 'Refined', 150, 'images/13.png', 0, NULL),
-('Lady Meow-Meow', 'あめしょさん', 'American Shorthair', 'Diva', 100, 'images/11.png', 0, NULL),
-('Mr.Meowgi', 'おさむらいさん', 'Ronin', 'Mentoring', 250, 'images/10.png', 0, NULL),
-('Ms. Fortune', 'こいこいさん', 'Gold', 'Charismatic', 20, 'images/6.png', 0, NULL),
-('Ramses the Great', 'すふぃんさん', 'Sphinx', 'Riddler', 230, 'images/14.png', 0, NULL),
-('Saint Purrtrick', 'ねこまたさん', 'Ethereal', 'Awe-Inspiring', 222, 'images/5.png', 0, NULL),
-('Sassy Fran', 'かふぇさん', 'Waitress', 'Enthusiastic', 180, 'images/15.png', 0, NULL),
-('Senor Don Gato', 'ながぐつさん', 'Mustachioed', 'Scheming', 30, 'images/2.png', 0, NULL),
-('Tubbs', 'まんぞくさん', 'Fluffy', 'Finicky Feaster', 130, 'images/9.png', 0, NULL),
-('Xerxes IX', 'ぷりんすさん', 'Persian', 'Regal', 70, 'images/3.png', 0, NULL);
+INSERT INTO `chat` (`nom_chat`, `nom_japonais`, `description`, `caractere`, `niveau`, `image`, `type_chat`, `login`, `couleur`) VALUES
+('Bandit', 'さびがらさん', 'Tortoiseshell', 'Wild at Heart', 180, 'images/Bandit.png', 0, NULL, ''),
+('Billy the Kitten', 'きっどさん', 'Western Wear', 'Nihilistic', 250, 'images/Billy-O.png', 0, NULL, ''),
+('Bob the Cat', 'やまねこさん', 'Adventurer', 'Outdoorsy', 40, 'images/Bob_the_Cat.png', 0, NULL, ''),
+('Bolt', 'きじとらさん', 'Brown Tabby', 'Insatiable', 140, 'images/Bolt.png', 0, NULL, ''),
+('Breezy', 'しろきじさん', 'Brown & White Tabby', 'Laid Back', 30, 'images/Breezy.png', 0, NULL, ''),
+('Callie', 'みけさん', 'Calico', 'Carefree', 50, 'images/Callie.png', 0, NULL, ''),
+('Chairman Meow', 'なべねこさん', 'Camouflage', 'Boorish', 111, 'images/Chair.png', 0, NULL, ''),
+('Cocoa', 'ちゃはちさん', 'Brown Tuxedo', 'Indecisive', 45, 'images/Cocoa.png', 0, NULL, ''),
+('Conductor Whiskers', 'えきちょうさん', 'Railway Uniform', 'Vigilant', 50, 'images/N-D.png', 0, NULL, ''),
+('Dottie', 'しろみけさん', 'White Calico', 'Friendly', 195, 'images/Dottie.png', 0, NULL, ''),
+('Fred', 'ちゃとらさん', 'Orange Tabby', 'Lady-Killer', 150, 'images/Fred.png', 0, NULL, ''),
+('Frosty', 'ゆきねこさん', 'Straw Coat', 'Sensitive', 5, 'images/Frosty.png', 0, NULL, ''),
+('Gabriel', 'はちわれさん', 'Tuxedo', 'Diligent', 150, 'images/Gabriel.png', 0, NULL, ''),
+('Ginger', 'あかげさん', 'Red & White Mitts', 'Bashful', 60, 'images/Ginger.png', 0, NULL, ''),
+('Gozer', 'とーびーさん', 'Tortoiseshell Tabby', 'Sore Loser', 155, 'images/N-D.png', 0, NULL, ''),
+('Guy Furry', 'びすとろさん', 'Apron', 'Artisan', 30, 'images/Guy.png', 0, NULL, ''),
+('Joe DiMeowgio', 'たてじまさん', 'Baseball Jersey', 'Team Player', 28, 'images/Joe_DiMeowgio.png', 0, NULL, ''),
+('Kathmandu', 'まろまゆさん', 'Hunting Coat', 'Refined', 150, 'images/Kathmandu.png', 0, NULL, ''),
+('Lady Meow-Meow', 'あめしょさん', 'American Shorthair', 'Diva', 100, 'images/Meow.png', 0, NULL, ''),
+('Lexy', 'はいしろさん', 'Grey & White', 'Expensive Tastes', 100, 'images/Lexy.png', 0, NULL, ''),
+('Mack', 'しろとらさん', 'White Mackerel', 'Determined', 130, 'images/Mack.png', 0, NULL, ''),
+('Marshmallow', 'ぽいんとさん', 'Pointed', 'Aloof', 170, 'images/Marshmellow.png', 0, NULL, ''),
+('Misty', 'さばとらさん', 'Mackerel Tabby', 'Lazy', 160, 'images/Misty.png', 0, NULL, ''),
+('Mr.Meowgi', 'おさむらいさん', 'Ronin', 'Mentoring', 250, 'images/Meowgi.png', 0, NULL, ''),
+('Ms. Fortune', 'こいこいさん', 'Gold', 'Charismatic', 20, 'images/Ms_Fortune.png', 0, NULL, ''),
+('Patches', 'ぶちさん', 'Orange Patches', 'Jealous', 80, 'images/Patches.png', 0, NULL, ''),
+('Peaches', 'くりーむさん', 'Tan & Orange', 'Capricious', 45, 'images/Peaches.png', 0, NULL, ''),
+('Pepper', 'おっどさん', 'Black & Odd Eyes', 'Shy', 165, 'images/N-D.png', 0, NULL, ''),
+('Pickles', 'しろさばさん', 'Gray & White Tabby', 'Faint-Hearted', 0, 'images/Pickles.png', 0, NULL, ''),
+('Princess', 'むぎわらさん', 'Striped Torbie', 'Ditzy', 125, 'images/Princess.png', 0, NULL, ''),
+('Pumpkin', 'しろちゃとらさん', 'Orange & White Tabby', 'Spacey', 90, 'images/Pumpkin.png', 0, NULL, ''),
+('Ramses the Great', 'すふぃんさん', 'Sphinx', 'Riddler', 230, 'images/N-D.png', 0, NULL, ''),
+('Rascal', 'はいはちさん', 'Grey Tuxedo', 'Selfish', 140, 'images/Rascal.png', 0, NULL, ''),
+('Saint Purrtrick', 'ねこまたさん', 'Ethereal', 'Awe-Inspiring', 222, 'images/Saint.png', 0, NULL, ''),
+('Sassy Fran', 'かふぇさん', 'Waitress', 'Enthusiastic', 180, 'images/Sassy_Fran.png', 0, NULL, ''),
+('Senor Don Gato', 'ながぐつさん', 'Mustachioed', 'Scheming', 30, 'images/N-D.png', 0, NULL, ''),
+('Shadow', 'はいいろさん', 'Solid Grey', 'Peculiar', 50, 'images/Shadow.png', 0, NULL, ''),
+('Smokey', 'くろねこさん', 'Solid Black', 'Hot and Cold', 140, 'images/Smokey.png', 0, NULL, ''),
+('Snowball', 'しろねこさん', 'Solid White', 'Mellow', 80, 'images/Snowball.png', 0, NULL, ''),
+('Socks', 'くつしたさん', 'Black & White Mitts', 'Adventurous', 70, 'images/Socks.png', 0, NULL, ''),
+('Speckles', 'くろぶちさん', 'Black Patches', 'Lonely', 40, 'images/Speckles.png', 0, NULL, ''),
+('Spooky', 'はいさびさん', 'Grey Tortoiseshell', 'Reserved', 35, 'images/Spooky.png', 0, NULL, ''),
+('Spots', 'しろくろさん', 'Black & White', 'Joker', 75, 'images/Spots.png', 0, NULL, ''),
+('Spud', 'あかさびさん', 'Red Tortoiseshell', 'Cautious', 80, 'images/Spud.png', 0, NULL, ''),
+('Sunny', 'とびみけさん', 'Turkish Calico', 'Mischievous', 120, 'images/Sunny.png', 0, NULL, ''),
+('Tabitha', 'しまみけさん', 'Calico Tabby', 'Leisurely', 40, 'images/Tabitha.png', 0, NULL, ''),
+('Tubbs', 'まんぞくさん', 'Fluffy', 'Finicky Feaster', 130, 'images/N-D.png', 0, NULL, ''),
+('Willie', 'くろとらさん', 'Black Tabby', 'Crafty', 75, 'images/Willie.png', 0, NULL, ''),
+('Xerxes IX', 'ぷりんすさん', 'Persian', 'Regal', 70, 'images/Xerxes.png', 0, NULL, '');
 
 -- --------------------------------------------------------
 
@@ -98,8 +131,9 @@ CREATE TABLE IF NOT EXISTS `compte` (
 --
 
 INSERT INTO `compte` (`login`, `motdepasse`, `idutilisateur`, `email`) VALUES
-('toast', 'toast', 2, 'toast@toast.fr'),
-('toto', 'toto', 2, '');
+('admin', 'PGytuvRI/Jmicfl8uOgxqQ==', 2, ''),
+('aimbarre', 'F35sxPg9zDzMzTzWjMVEww==', 2, 'toast@gmail.com'),
+('toto', '5vEcAmWhfxiU+PnC+wdnkw==', 2, '');
 
 -- --------------------------------------------------------
 
