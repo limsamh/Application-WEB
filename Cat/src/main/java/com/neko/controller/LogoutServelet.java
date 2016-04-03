34b0e4f14f7c38e9e68d34903180e68c055d6fe6
mbarrelinovic <LM@MBARRE-PC>
2016-04-03 14:01:12 +0200

    t

---
+++ Cat/src/main/java/com/neko/controller/LogoutServelet.java
@@ -0,0 +1,27 @@
+package com.neko.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServelet extends HttpServlet 
+{
+
+	
+	protected void doGet (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		res.setContentType("text/html");  
+		PrintWriter out=res.getWriter();  
+		
+		req.getRequestDispatcher("logout.jsp").include(req, res);  
+		
+		HttpSession session=req.getSession();  
+		session.invalidate();  
+	}
+
+}
